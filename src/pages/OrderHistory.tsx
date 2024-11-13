e2bbdada25449647cd21a1a305ec783415a5761f
Samuel Ogbaje <samuelogbaje@gmail.com>
2024-11-13 23:11:22 +0100

    Test-commit

--- src/pages/OrderHistory.tsx
+++ src/pages/OrderHistory.tsx
@@ -1,9 +1,15 @@
-export default function OrderHistory() {    
-    return (
-      <>
-      <div>
-        <h1>Order History</h1>
+import React from "react";
+import { GiNotebook } from "react-icons/gi";
+
+export default function OrderHistory() {
+  return (
+    <>
+      <div className="bg-amber-700 p-4 border border-green-500 h-screen">
+        <div className="flex flex-row items-center gap-4 rounded-md bg-rose-300">
+          <GiNotebook />
+          <h1>Order History</h1>
+        </div>
       </div>
-      </>
-    );
-    }   
+    </>
+  );
+}
