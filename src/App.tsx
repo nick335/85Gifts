1a64dd7aa6958d3f453f0d1e07e6d9da6aa941e5
Desiree Chukwuji <queendesi06@gmail.com>
2025-02-05 23:17:52 +0100

    login page done

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,38 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./layouts/AppLayout";
-import './App.css';
-import LandingPage from "./pages/LandingPage";
-import HomePage from "./pages/HomePage";
-import Cart from "./pages/Cart";
-import OrderHistory from "./pages/OrderHistory";
-import Messages from "./pages/Messages";
-import Gifts from "./pages/Gifts";
-import Settings from "./pages/Settings";
-import Logout from "./pages/Logout";
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Layout from "./layouts/AppLayout";
+// import './App.css';
+// import LandingPage from "./pages/LandingPage";
+// import HomePage from "./pages/HomePage";
+// import Cart from "./pages/Cart";
+// import OrderHistory from "./pages/OrderHistory";
+// import Messages from "./pages/Messages";
+// import Gifts from "./pages/Gifts";
+// import Settings from "./pages/Settings";
+// import Logout from "./pages/Logout";
+import Login from "./pages/LoginPage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/*  Authenticated routes */}
-        <Route element={<Layout />}>
-        <Route path="/HomePage" element={<HomePage />} />
-          <Route path="/Gifts" element={<Gifts />} /> 
-           <Route path="/Messages" element={<Messages />} />
-           <Route path="/Cart" element={<Cart />} />
-           <Route path="/OrderHistory" element={<OrderHistory />} />
-           <Route path="/Settings" element={<Settings />} />
-           <Route path="/Logout" element={<Logout />} />
-        </Route>
+    // <Router>
+    //   <Routes>
+    //     {/*  Authenticated routes */}
+    //     <Route element={<Layout />}>
+    //     <Route path="/HomePage" element={<HomePage />} />
+    //       <Route path="/Gifts" element={<Gifts />} /> 
+    //        <Route path="/Messages" element={<Messages />} />
+    //        <Route path="/Cart" element={<Cart />} />
+    //        <Route path="/OrderHistory" element={<OrderHistory />} />
+    //        <Route path="/Settings" element={<Settings />} />
+    //        <Route path="/Logout" element={<Logout />} />
+    //     </Route>
 
-        {/* Landing Page Routes  */}
-          <Route path="/" element={<LandingPage />} />
-        {/* <Route path="/login" element={<Login />} /> */}
-      </Routes>
-    </Router>
+    //     {/* Landing Page Routes  */}
+    //       <Route path="/" element={<LandingPage />} />
+    //     {/* <Route path="/login" element={<Login />} /> */}
+    //   </Routes>
+    // </Router>
+    <Login />
   );
 }
 
