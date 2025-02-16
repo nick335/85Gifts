7341585bcc323c091442066273e463cb14d3008b
Noble Unyimeabasi Udoesit <noblenoble2014@gmail.com>
2025-02-16 22:05:56 +0100

    feat:user-authentication

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,7 +1,109 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios"
 import logo from "../assets/logo.png";
 import Frame285 from "../assets/Frame285.png";
 
 export default function Signup() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    userName: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [errors, setErrors] = useState<{
+    firstName?: string;
+    lastName?: string;
+    userName?: string;
+    email?: string;
+    phoneNumber?: number;
+    password?: string;
+    confirmPassword?: string;
+  }>({});
+  const [valid, setValid] = useState(true);
+  const navigate = useNavigate();
+
+  const handleSignup = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    let isValid = true;
+    const validationErrors: {
+      firstName?: string;
+      lastName?: string;
+      userName?: string;
+      email?: string;
+      phoneNumber?: number;
+      password?: string;
+      confirmPassword?: string;
+    } = {};
+
+    if (!formData.firstName.trim()) {
+      isValid = false;
+      validationErrors.firstName = "First Name is required";
+    }
+    if (!formData.lastName.trim()) {
+      isValid = false;
+      validationErrors.lastName = "Last Name is required";
+    }
+    if (!formData.userName.trim()) {
+      isValid = false;
+      validationErrors.userName = "Username is required";
+    } else if (formData.userName.length < 6) {
+      isValid = false;
+      validationErrors.userName = "Username must be at least 6 characters";
+    }
+    if (!formData.email.trim()) {
+      isValid = false;
+      validationErrors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      isValid = false;
+      validationErrors.email = "Invalid email format";
+    }
+    // if (formData.phoneNumber === null || typeof formData.phoneNumber !== 'number') {
+    //   isValid = false;
+    //   validationErrors.phoneNumber = "Phone Number is required";
+    // }else if((formData.phoneNumber).length > 11 || (formData.phoneNumber).length < 11) {
+    //   isValid = false;
+    //   validationErrors.phoneNumber = "Phone Number is not valid";
+    // }
+    if (!formData.password.trim()) {
+      isValid = false;
+      validationErrors.password = "Password required";
+    } else if (formData.password.length < 8) {
+      isValid = false;
+      validationErrors.password = "Password must be at least 8 characters long";
+    }
+    if (formData.confirmPassword !== formData.password) {
+      isValid = false;
+      validationErrors.confirmPassword = "Password do not match";
+    }
+
+    setErrors(validationErrors);
+    setValid(isValid);
+
+    if (isValid) {
+      try{
+        const response = await axios.post(
+          'https://eight5gifts-be.onrender.com/api/user/signup',
+          formData
+        );
+
+      if(response.status === 201){
+        localStorage.setItem("token", response.data.token);
+        
+        alert('Registered successfully');
+        navigate("/homepage")
+      }
+    }catch(err) {
+      console.error("Signup error: ", err);
+      alert("Signup failed. Please try again");
+    }
+  }
+};
+
   return (
     <>
       <div className="signup-page flex flex-col sm:flex-row lg:flex-row">
@@ -13,46 +115,129 @@ export default function Signup() {
             <p className="text-xl font-semibold text-center sm:text-left">
               Hi there! Let's create your account and get you going.
             </p>
-            <form className="flex flex-col items-center sm:items-start mt-5 ml-6">
+            <form
+              className="flex flex-col items-center sm:items-start mt-5 ml-6"
+              onSubmit={handleSignup}
+            >
+              {valid ? (
+                <></>
+              ) : (
+                <span className="text-sm text-red-600">
+                  {errors.firstName};
+                </span>
+              )}
               <input
                 type="text"
+                name="firstName"
                 placeholder="Enter your first name"
                 className="w-full sm:w-1/2 p-3 border rounded-xl mb-4 outline-none"
+                value={formData.firstName}
+                required
+                onChange={(e) =>
+                  setFormData({ ...formData, firstName: e.target.value })
+                }
               />
+              {valid ? (
+                <></>
+              ) : (
+                <span className="text-sm text-red-600">{errors.lastName};</span>
+              )}
               <input
                 type="text"
+                name="lastName"
                 placeholder="Enter your last name"
                 className="w-full sm:w-1/2 p-3 border rounded-xl mb-4 outline-none"
+                required
+                value={formData.lastName}
+                onChange={(e) =>
+                  setFormData({ ...formData, lastName: e.target.value })
+                }
               />
+              {valid ? (
+                <></>
+              ) : (
+                <span className="text-sm text-red-600">{errors.userName};</span>
+              )}
               <input
                 type="text"
+                name="username"
                 placeholder="Enter your username"
                 className="w-full sm:w-1/2 p-3 border rounded-xl mb-4 outline-none"
+                required
+                value={formData.userName}
+                onChange={(e) =>
+                  setFormData({ ...formData, userName: e.target.value })
+                }
               />
+              {valid ? (
+                <></>
+              ) : (
+                <span className="text-sm text-red-600">{errors.email};</span>
+              )}
               <input
                 type="email"
+                name="email"
                 placeholder="Enter your email address"
                 className="w-full sm:w-1/2 p-3 border rounded-xl mb-4 outline-none"
+                required
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
               />
-              <input
-                type="number"
+
+              {/* <input
+                type="tel"
+                name="phoneNumber"
                 placeholder="Enter your phone number"
                 className="w-full sm:w-1/2 p-3 border rounded-xl mb-4 outline-none"
-              />
+                required
+                value={formData.phoneNumber}
+                onChange={(e) =>
+                  setFormData({ ...formData, phoneNumber: e.target.value })
+                }
+              /> */}
+              {valid ? (
+                <></>
+              ) : (
+                <span className="text-sm text-red-600">{errors.password};</span>
+              )}
               <input
                 type="password"
+                name="password"
                 placeholder="Create a strong password"
                 className="w-full sm:w-1/2 p-3 border rounded-xl mb-4 outline-none"
+                required
+                value={formData.password}
+                onChange={(e) =>
+                  setFormData({ ...formData, password: e.target.value })
+                }
               />
+
+              {valid ? (
+                <></>
+              ) : (
+                <span className="text-sm text-red-600">
+                  {errors.confirmPassword};
+                </span>
+              )}
               <input
                 type="password"
+                name="confirmPassword"
                 placeholder="Re-enter your password"
                 className="w-full sm:w-1/2 p-3 border rounded-xl mb-4 outline-none"
+                required
+                value={formData.confirmPassword}
+                onChange={(e) =>
+                  setFormData({ ...formData, confirmPassword: e.target.value })
+                }
               />
+
               <button className="bg-black text-white rounded-xl w-full sm:w-1/2 px-10 py-5">
                 Submit
               </button>
             </form>
+            <p className="mt-5 mr-10">Already have an account? <Link to="/">Login</Link></p>
           </div>
         </div>
         <div
