2b43d3271fc78ff9b1f2ca0643029ef64862ac2e
auscay <112304998+auscay@users.noreply.github.com>
2025-05-14 08:42:43 +0100

    Fix/UI (#64)
    * fix ui errors
    
    * mobile responsiveness fix
    
    * removed uncessary comments
    
    ---------
    
    Co-authored-by: chuma-beep <wisboynelson123@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./layouts/AppLayout";
 import './App.css';
 import LandingPage from "./pages/LandingPage";
@@ -29,63 +29,61 @@ import AdminGiftsPage from "./pages/admin/VeiwAllGifts.tsx";
 import ErrorPage from "./pages/ErrorPage";
 import Invoice from "./pages/ViewInvoice";
 import Orders from "./pages/Orders";
+// import NotFound from "@/components/NotFound.tsx";
+import { ErrorBoundary } from "react-error-boundary";
+import ProtectedRoute from "@/components/ProtectedRoute.tsx";
 import { Toaster } from "sonner";
 
-// Protected Route Wrapper
-const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const authToken = localStorage.getItem("authToken");
-  return authToken ? children : <Navigate to="/login" />;
-};
 
 function App() {
   return (
-    <>
-    <Router>
-       <Routes>
-         {/* Authenticated Routes (Require Login) */}
-        <Route element={<ProtectedRoute><Layout /></ProtectedRoute>}>
-        <Route path="/HomePage" element={<HomePage />} />
-        <Route path="/Gifts" element={<Gifts />} />
-        <Route path="/gift/:_id" element={<GiftDetail />} />
-        <Route path="/GiftCuration" element={<GiftCuration />} />
-        <Route path="/Cart" element={<Cart />} />
-        <Route path="/Invoice/:invoiceNumber" element={<Invoice />} />
-        <Route path="/OrderHistory" element={<OrderHistory />} />
-        <Route path="/Orders" element={<Orders />} />
-        <Route path="/Settings" element={<Settings />} />
-        </Route>
+    <ErrorBoundary FallbackComponent={ErrorPage}>
+      <Router>
+        <Routes>
+          {/* Authenticated Routes (Require Login) */}
+          <Route element={<ProtectedRoute><Layout /></ProtectedRoute>}>
+            <Route path="/HomePage" element={<HomePage />} />
+            <Route path="/Gifts" element={<Gifts />} />
+            <Route path="/gift/:_id" element={<GiftDetail />} />
+            <Route path="/GiftCuration" element={<GiftCuration />} />
+            <Route path="/Cart" element={<Cart />} />
+            <Route path="/Invoice/:invoiceNumber" element={<Invoice />} />
+            <Route path="/OrderHistory" element={<OrderHistory />} />
+            <Route path="/Orders" element={<Orders />} />
+            <Route path="/Settings" element={<Settings />} />
+          </Route>
 
-        {/* Public Routes */}
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/Signup" element={<Signup />} />
-        <Route path="/VerifyEmail" element={<VerifyEmail />} />
-        <Route path="/reset-password" element={<CustomerResetPassword />} />
+          {/* Public Routes */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/VerifyEmail" element={<VerifyEmail />} />
+          <Route path="/reset-password" element={<CustomerResetPassword />} />
 
-        {/* Admin Routes */}
-        <Route path="/admin/login" element={<AdminLogin />} />
-        <Route path="/admin/reset" element={<AdminReset />} />
-          
+          {/* Admin Routes */}
+          <Route path="/admin/login" element={<AdminLogin />} />
+          <Route path="/admin/reset" element={<AdminReset />} />
 
 
-        {/* Admin Layout */}
-        <Route element={<AdminLayout />}>
-    <Route path="/adminpage" element={<AdminDashBoard />} />
-    <Route path="/users" element={<Users />} />
-    <Route path="/orders" element={<OrdersTab />} />
-    <Route path="/admingiftspage" element={<AdminGiftsPage />} />
-    <Route path="/transactions" element={<Transactions />} />
-    </Route>
+
+          {/* Admin Layout */}
+          <Route element={<AdminLayout />}>
+            <Route path="/adminpage" element={<AdminDashBoard />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/adminorders" element={<OrdersTab />} />
+            <Route path="/admingiftspage" element={<AdminGiftsPage />} />
+            <Route path="/transactions" element={<Transactions />} />
+          </Route>
 
 
           {/* Catch-all route */}
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </Router>
-      <Toaster  position="top-center" richColors/>
-    </>
+      <Toaster position="top-center" richColors />
+    </ErrorBoundary>
   );
- 
+
 }
 
 export default App;
